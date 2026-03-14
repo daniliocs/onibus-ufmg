b086b56b6568cfa8823328417622893a1e3940fb
daniliocs <daniliocs@ufmg.br>
2026-03-14 12:28:32 -0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'onibus-ufmg-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  // O Leaflet precisa de internet, então deixamos de fora para o app não travar offline
+];
+
+// Instala o Service Worker e salva os arquivos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Intercepta as requisições para funcionar sem internet
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
