ac22ab79b172f0cb6db9637641cbbea5bb04e555
daniliocs <daniliocs@ufmg.br>
2026-03-17 09:42:51 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'onibus-ufmg-v12'; // Mude para v3 na próxima vez que alterar o site
+const CACHE_NAME = 'onibus-ufmg-v13'; // Mude para v3 na próxima vez que alterar o site
 const ASSETS = [
   './',
   './index.html',
